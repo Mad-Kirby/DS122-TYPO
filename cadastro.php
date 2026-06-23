c75ee2db31e5dd6d9e330ba143478c4c9c28319c
Henrique Santini <hsantini.05+github@gmail.com>
2026-06-23 00:42:17 -0300

    adicionado campos de erro e requisitos de senha no cadastro

--- cadastro.php
+++ cadastro.php
@@ -11,6 +11,7 @@ $step = $_GET["step"] ?? "dados";
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/autenticar.css" />
+  <script src="js/cadastro.js" defer></script>
 </head>
 <body>
   <main class="auth-page">
@@ -27,13 +28,22 @@ $step = $_GET["step"] ?? "dados";
         <form class="auth-form" action="login.php" method="post">
         <div class="field">
             <input type="password" id="senha" name="senha" placeholder="Senha:"/>
+            <ul class="msg-erro req-senha">
+              <span>Senha deve possuir:</span>
+              <li>ao menos 8 caracteres;</li>
+              <li>letras maiúsculas;</li>
+              <li>letras minúsculas;</li>
+              <li>números;</li>
+              <li>ao menos um caractere especial.</li>
+            </ul>
         </div>
 
         <div class="field">
             <input type="password" id="confirmar-senha" name="confirmar_senha" placeholder="Confirmar Senha:"/>
+            <p class="msg-erro"></p>
         </div>
 
-          <button type="submit" class="btn-submit">Criar Conta</button>
+          <button type="submit" class="btn-submit" id="btn-cadastro">Criar Conta</button>
         </form>
 
       <?php } else { ?>
@@ -41,13 +51,15 @@ $step = $_GET["step"] ?? "dados";
         <form class="auth-form" action="cadastro.php?step=senha" method="post">
           <div class="field">
             <input type="text" id="nome" name="nome" placeholder="Nome:" />
+            <p class="msg-erro"></p>
           </div>
 
           <div class="field">
             <input type="email" id="email" name="email" placeholder="E-mail:" />
+            <p class="msg-erro"></p>
           </div>
 
-          <button type="submit" class="btn-submit">Continuar</button>
+          <button type="submit" class="btn-submit" id="btn-continuar">Continuar</button>
         </form>
 
       <?php } ?>
