176e20073653bcbd0af81a4a4fb9fea40e764105
GabeGJM <gabriel.jaremczyk@gmail.com>
2026-06-25 00:47:49 -0300

    feat(historico): estrutura HTML base da tela de Histórico
    Marcação estrutural da tela de histórico. Contendo container pai e dois containers filhos, que contem texto para exibir o nome do jogador(id="nome-jogador"), exibir a data da tentativa(id="data-historico") e exibir pontos da tentativa(id="pts-historico"). No topo da página foi adicionado o botão voltar(id="voltar-historico").

--- historico.php
+++ historico.php
@@ -1,11 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HISTÓRICO</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Histórico</title>
+  <link rel="stylesheet" href="css/ligas.css" />
 </head>
 <body>
-    <h1>GABRIEL O HTML DO HISTORICO DE PARTIDAS É AQUI</h1>
+  <main class="container">
+    <button id="voltar-historico"class="btn back-button">← Voltar</button>
+
+    <section class="historico-card">
+  <h1 class="historico-title">Histórico</h1>
+
+  <div class="historico-bloco">
+    <div id="nome-jogador" class="historico-jogador">Jogador</div>
+
+    <table class="historico-tabela">
+      <thead>
+        <tr>
+          <th>Data</th>
+          <th>Pontuação</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td id="data-historico">data</td>
+          <td id="pts-historico">pts</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</section>
+  </main>
 </body>
 </html>
